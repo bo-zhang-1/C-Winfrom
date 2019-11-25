d14ccfc2ba29aef579dbedc8158bbdcc5d357c36
zhangbo <1910708085@qq.com>
2019-11-25 22:26:16 +0800

    thread Safe  test

---
+++ ThreadSafeTest/delegateFrom.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadSafe
+{
+    public partial class delegateFrom : Form
+    {
+        private delegate void SafeCallMoteod();
+
+        public delegateFrom()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                var d = new SafeCallMoteod(SetText);
+                textBox1.Invoke(d, null);
+            }
+            else
+            {
+                SetText();
+            }
+            Thread myThread = new Thread(new ThreadStart(SetText));
+            myThread.Start();
+        }
+
+        private void SetText()
+        {
+            textBox1.Text = "this is MyText!";
+        }
+
+        ///// <summary>
+        ///// 应用程序的主入口点。
+        ///// </summary>
+        //[STAThread]
+        //private static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new delegateFrom());
+        //}
+    }
+}
---
+++ ThreadSafeTest/frmBackgroundWorker.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadSafe
+{
+    public partial class frmBackgroundWorker : Form
+    {
+        public frmBackgroundWorker()
+        {
+            InitializeComponent();
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+            btnCancle.Enabled = true;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                if ((sender as BackgroundWorker).CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    (sender as BackgroundWorker).ReportProgress(i * 5);
+                    e.Result = txtValue.Text + i.ToString();
+                }
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                txtValue.Text = "Canceled";
+            }
+            else
+            {
+                // Finally, handle the case where the operation
+                // succeeded.
+                txtValue.Text = e.Result.ToString();
+            }
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            btnCancle.Enabled = false;
+            backgroundWorker1.CancelAsync();
+            btnStart.Enabled = true;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //获取异步任务执行的百分比
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmBackgroundWorker());
+        }
+    }
+}
